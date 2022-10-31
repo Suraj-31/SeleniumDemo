80155c85be11dd5ec8c597c942d2f1df002d1ce5
Megha <Megha@Megha>
2022-10-31 19:27:33 +0530

    first commit

---
+++ demo1/src/main/java/com/actitime/selenium1.java
@@ -0,0 +1,11 @@
+package com.actitime;
+
+public class selenium1 {
+public  void login()
+{
+	
+System.out.println("this is my first project");
+System.out.println("this is my first project");
+}
+}
+
---
+++ demo1/src/test/java/com/testscript/selenium2.java
@@ -0,0 +1,8 @@
+package com.testscript;
+
+public class selenium2 {
+public void signup()
+{
+	System.out.println("this is my beggining of the program");
+}
+}
